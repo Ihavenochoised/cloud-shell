b34f374d3f4e0c2cd190f6c8dfc1ee4ec435d954
Xiong Tianrui <xiongtianrui195@gmail.com>
2026-03-07 12:53:38 +0800

    Update repository structure after forking template

--- renderBuild.sh
+++ renderBuild.sh
@@ -7,4 +7,7 @@ npm install
 echo "📍 List of installed dependencies:"
 npm ls
 
+echo "🔐 Generating development certificate..."
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/CN=localhost"
+
 echo "✅️ Dependencies installed, ready to start!"
